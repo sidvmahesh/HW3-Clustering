5207b2f013323fa0c86a70c612efeed52a4d4329
sidvmahesh <siddharth.mahesh@ucsf.edu>
2024-01-31 17:58:49 -0800

    added ARI

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -4,12 +4,11 @@ import cluster
 from cluster import Silhouette
 from cluster import KMeans
 from cluster.utils import make_clusters
-from sklearn.metrics import silhouette_samples
+from sklearn.metrics.cluster import adjusted_rand_score
 
 def test_kmeans():
     X, labels = make_clusters(n=1000, m=20, k=3)
     km = KMeans(k=3, tol = 1e-6, max_iter = 400)
     km.fit(X)
     cluster_membership = km.predict(X)
-    scores = Silhouette.score(X, labels)
-    assert ground_truth == scores
+    assert adjusted_rand_score(labels, cluster_membership) > 0.8
