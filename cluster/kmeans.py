2db6ec8d1fcf7f0e75bdc5497aeb4b75bbaefa10
sidvmahesh <siddharth.mahesh@ucsf.edu>
2024-01-31 22:41:52 -0800

    fixed kmeans syntax

--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -62,7 +62,7 @@ class KMeans:
             # Finally, check if the clustering has converged
             cumulative_error = 0
             for i in range(len(centroids)):
-                cumulative_error += cdist(np.reshape(centroids[i], (1, len(centroids[i]))), np.reshape(self.centroids[i], (1, len(self.centroids[i]))))
+                cumulative_error = cumulative_error + cdist([centroids[i]], [self.centroids[i]])[0][0]
             if cumulative_error < self.tol:
                 break # Early convergence
             self.centroids = centroids
