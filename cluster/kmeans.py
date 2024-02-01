7dd0b33dd5d0d74a3810f280afb68a458ce463d9
sidvmahesh <siddharth.mahesh@ucsf.edu>
2024-01-31 16:00:50 -0800

    fixed dims

--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -57,7 +57,7 @@ class KMeans:
                 #for k in range(centroids.shape[0]):
                 #    centroid_distances[k] = cdist(mat[obs], centroids[k])
                 print("mat[obs]: ", mat[obs].shape)
-                centroid_distances = cdist(mat[obs], centroids)
+                centroid_distances = cdist(np.reshpae(mat[obs], (1, 20)), centroids)
                 cluster_membership[obs] = np.argmin(centroid_distances)
                 self.centroid_distances[obs] = centroid_distances
             # Now, re-calculate centroid locations
