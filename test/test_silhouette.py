d2f79b9d8bc82d8fd63e5218c1e16748aa823af0
sidvmahesh <siddharth.mahesh@ucsf.edu>
2024-01-31 19:13:54 -0800

    removed prints

--- test/test_silhouette.py
+++ test/test_silhouette.py
@@ -10,11 +10,11 @@ def test_silhouette_score():
     X, labels = make_clusters(n=1000, m=20, k=3)
     ground_truth = silhouette_samples(X, labels)
     scores = Silhouette().score(X, labels)
-    print("ground_truth: ", ground_truth)
-    print("scores: ", scores)
+    #print("ground_truth: ", ground_truth)
+    #print("scores: ", scores)
     assert(len(ground_truth) == len(scores))
     for i in range(len(ground_truth)):
-        if abs(ground_truth[i] - scores[i]) > 0.02:
+        if abs(ground_truth[i] - scores[i]) > 0.01:
             print("ERROR: observation", i, "has incorrect silhouette score")
             assert False
     assert True
